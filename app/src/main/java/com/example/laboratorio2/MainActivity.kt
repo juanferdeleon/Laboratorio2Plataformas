22fb05115353c90d60e488631aff22e4c8d8b749
Juan De Leon <dele17822@uvg.edu.gt>
2019-03-30 18:47:41 -0600

    Diseno

--- app/src/main/java/com/example/laboratorio2/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio2/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.laboratorio2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newLap = findViewById<Button>(R.id.newlap)
+
+        newLap.setOnClickListener {
+            val intent = Intent(this, NewTurn::class.java)
+            startActivity(intent)
+        }
+
     }
 }
